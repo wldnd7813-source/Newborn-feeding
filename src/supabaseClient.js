f9a913e19a902cbfe77cd246835052709392a53f
Park Ji Woong <wldnd7813@gmail.com>
2026-01-10 21:54:38 +0900

    Supabase URL 수정

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://innkwfssvezgkoyzsrzm.supabase.co'
+const supabaseUrl = 'https://innkwfssvezqkoyzsrzm.supabase.co'
 const supabaseAnonKey = 'sb_publishable_-yT_CC6cTfL0ll5M10DdaA_JyXKdoyF'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
